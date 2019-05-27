8865e2474e95104206a6fc76c89fab4e54b77351
Riyaz <riyaz.ahmed@nykaa.com>
2019-05-27 22:54:55 +0530

    checks for corresponding least and most value and only applies for one

--- src/reducers/answersReducer.js
+++ src/reducers/answersReducer.js
@@ -9,9 +9,24 @@ const answersReducer = (state = initialState, action) => {
 
   switch (action.type) {
     case answersConstants.ADD_MOST:
-    // TODO check if the correspoding least data's Id is the same as the payload.data.id
-    // If so then then remove the least key
-    // and wise versa
+      // TODO check if the correspoding least answers's Id is the same as the payload.data.id
+      // If so then then remove the least key
+      // and wise versa
+      if (
+        state.answers[payload.key]
+        && state.answers[payload.key].least
+        && state.answers[payload.key].least.id === payload.data.id
+        ) {
+        return {
+          ...state,
+          answers: {
+            ...state.answers,
+            [payload.key]: {
+              'most': payload.data
+            }
+          }
+        };
+      }
       return {
         ...state,
         answers: {
@@ -22,18 +37,37 @@ const answersReducer = (state = initialState, action) => {
           }
         }
       }
+
+
+
     case answersConstants.ADD_LEAST:
-    return {
-      ...state,
-      answers: {
-        ...state.answers,
-        [payload.key]: {
-          ...state.answers[payload.key],
-          'least': payload.data
+      if (
+        state.answers[payload.key]
+        && state.answers[payload.key].most
+        && state.answers[payload.key].most.id === payload.data.id
+        ) {
+        return {
+          ...state,
+          answers: {
+            ...state.answers,
+            [payload.key]: {
+              'least': payload.data
+            }
+          }
+        };
+      }
+
+      return {
+        ...state,
+        answers: {
+          ...state.answers,
+          [payload.key]: {
+            ...state.answers[payload.key],
+            'least': payload.data
+          }
         }
       }
-    }
-  default:
+    default:
       return state;
   }
 }
