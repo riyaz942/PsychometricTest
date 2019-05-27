8699d67ed1181e0b36c5631b8e2078df0fc5ece6
Riyaz <riyaz.ahmed@nykaa.com>
2019-05-28 00:49:31 +0530

    added some stuff

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import React from 'react';
-import { Modal, Button } from 'react-materialize';
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
 import styles from './app.module.scss';
 import Table from './components/table/table';
+import Snackbar from '@material-ui/core/Snackbar';
 
-function App() {
-const trigger = <Button>Open Modal</Button>;
+class App extends Component {
+  state ={
+    openSnackBar: false,
+  }
 
-return (
-    <div className={styles.app}>
-      <div className={styles.app_container}>
-        <Table />
-        {/* <Modal header="Modal Header" trigger={trigger}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </Modal> */}
+  handleClick = () => {
+    this.setState({
+      openSnackBar: true
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      openSnackBar: false,
+    })
+  }
+
+  render() {
+    const {openSnackBar} = this.state;
+
+    return (
+      <div className={styles.app}>
+        <div className={styles.app_container}>
+          <Table />
+          <Button onClick={this.handleClick}>Open simple snackbar</Button>
+          <Snackbar
+            variant="error"
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            open={openSnackBar}
+            onClose={this.handleClose}
+            autoHideDuration={6000}
+            ContentProps={{
+              'aria-describedby': 'message-id',
+            }}
+            message={<span id="message-id">Note archived</span>}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );  
+  }
 }
 
 export default App;
