a7f743c65101540e4d10bcdfb3e65518249b2fd7
Riyaz <riyaz.ahmed@nykaa.com>
2019-06-03 01:47:42 +0530

    Updates

---
+++ precache-manifest.f00e800cd20341978429caec178ac372.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bac06b638f8c652ef888475109ac2be",
+    "url": "/PsychometricTest/index.html"
+  },
+  {
+    "revision": "b602649dd50456464ec3",
+    "url": "/PsychometricTest/static/css/main.cfbfa4bb.chunk.css"
+  },
+  {
+    "revision": "096f6dfa1e1e4aa10356",
+    "url": "/PsychometricTest/static/js/2.d3207f78.chunk.js"
+  },
+  {
+    "revision": "b602649dd50456464ec3",
+    "url": "/PsychometricTest/static/js/main.7305ae51.chunk.js"
+  },
+  {
+    "revision": "9419361a8773841d4f29",
+    "url": "/PsychometricTest/static/js/runtime~main.e2d4147f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PsychometricTest/precache-manifest.56320f95a1dfe2bfcca5a88849bbdf54.js"
+  "/PsychometricTest/precache-manifest.f00e800cd20341978429caec178ac372.js"
 );
 
 self.addEventListener('message', (event) => {
